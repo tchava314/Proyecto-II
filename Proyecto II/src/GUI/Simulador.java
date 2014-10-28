0697362327fd828feddec9c38874b8fc946e8c7b
yalvar167 <yalvar167@ubuntu>
2014-10-28 00:13:25 -0700

    ..

--- Proyecto II/src/GUI/Simulador.java
+++ Proyecto II/src/GUI/Simulador.java
@@ -1,4 +1,5 @@
 package GUI;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -7,26 +8,73 @@ import javax.swing.*;
 public class Simulador extends JFrame implements ActionListener{
 	
 	JMenuItem nuevo,open,salir,borrar,save,help,about;
+	JButton and,nand,or,nor,not,xor,xnor;
+	ImageIcon imagen;
+	Icon icono;
 	public Simulador(){
-		JPanel panel1 = new JPanel();
 		//se crea un menu de barra
 		JMenuBar Barra= new JMenuBar();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(720,500);
+		setResizable(false);
 		setTitle("LogiCTec");
 		setLocationRelativeTo(null);
-		setLayout(new GridLayout(0,5));
+		setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JLabel text= new JLabel("Compuertas Logicas");
 		
-	    panel1.setLayout(new GridLayout(8,0,4,4));
-	    panel1.add(text);
-	    panel1.add(new JButton("AND"));
-	    panel1.add(new JButton("NAND"));
-	    panel1.add(new JButton("OR"));
-	    panel1.add(new JButton("NOR"));
-	    panel1.add(new JButton("NOT"));
-	    panel1.add(new JButton("XOR"));
-	    panel1.add(new JButton("XNOR"));
+		//se crea el boton and
+		JButton and=new JButton();
+		and.setBounds(0,0,140,60);
+		imagen= new ImageIcon("and.jpg");
+		icono=new ImageIcon(imagen.getImage().getScaledInstance(and.getWidth(),and.getHeight(),Image.SCALE_DEFAULT));
+		and.setIcon(icono);
+		
+		//se crea el boton nand
+		JButton nand=new JButton();
+		nand.setBounds(0,70,140,60);
+		imagen= new ImageIcon("nand.jpg");
+		icono=new ImageIcon(imagen.getImage().getScaledInstance(and.getWidth(),and.getHeight(),Image.SCALE_DEFAULT));
+		nand.setIcon(icono);
+		
+		//se crea el boton or
+		JButton or=new JButton();
+		or.setBounds(0,140,140,60);
+		imagen= new ImageIcon("or.jpg");
+		icono=new ImageIcon(imagen.getImage().getScaledInstance(and.getWidth(),and.getHeight(),Image.SCALE_DEFAULT));
+		or.setIcon(icono);
+				
+		//se crea el boton nor
+		JButton nor=new JButton();
+		nor.setBounds(0,210,140,60);
+		imagen= new ImageIcon("nor.png");
+		icono=new ImageIcon(imagen.getImage().getScaledInstance(and.getWidth(),and.getHeight(),Image.SCALE_DEFAULT));
+		nor.setIcon(icono);
+		
+		//se crea el boton not
+		JButton not=new JButton();
+		not.setBounds(0,280,140,60);
+		imagen= new ImageIcon("not.jpeg");
+		icono=new ImageIcon(imagen.getImage().getScaledInstance(and.getWidth(),and.getHeight(),Image.SCALE_DEFAULT));
+		not.setIcon(icono);
+				
+		//se crea el boton xor
+		JButton xor=new JButton();
+		xor.setBounds(0,350,140,60);
+		imagen= new ImageIcon("xor.jpg");
+		icono=new ImageIcon(imagen.getImage().getScaledInstance(and.getWidth(),and.getHeight(),Image.SCALE_DEFAULT));
+		xor.setIcon(icono);
+		
+		//se crea el boton xnor
+		JButton xnor=new JButton();
+		xnor.setBounds(0,420,140,60);
+		imagen= new ImageIcon("xnor.jpg");
+		icono=new ImageIcon(imagen.getImage().getScaledInstance(and.getWidth(),and.getHeight(),Image.SCALE_DEFAULT));
+		xnor.setIcon(icono);
+			
+		/*
+		JPanel panel1= new JPanel();
+		imagen= new ImageIcon("cuadricula.jpg");
+		icono=new ImageIcon(imagen.getImage().getScaledInstance(300,100,Image.SCALE_DEFAULT));
+		*/
 		
 	    //se crean submenus 
         JMenu fileMenu = new JMenu("Archivo");
@@ -65,11 +113,16 @@ public class Simulador extends JFrame implements ActionListener{
         opcMenu.add(help);
         opcMenu.add(about);
 		
-        
-
-	    add(panel1);
+        add(text);
+	    add(and);
+	    add(nand);
+	    add(or);
+	    add(nor);
+	    add(not);
+	    add(xor);
+	    add(xnor);
 	    setJMenuBar(Barra);
-		
+	    
 		setVisible(true);
 
 	}
@@ -77,6 +130,7 @@ public class Simulador extends JFrame implements ActionListener{
 	public static void main(String[] args) {
 		Simulador game= new Simulador();
 		game.pack();
+		game.setSize(760,530);
 	}
 
 	public void actionPerformed(ActionEvent e) {//sobreescribimos el metodo del listener
